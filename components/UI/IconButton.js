45b13a9dd1f1d45edfd9949f1924f22774fe956b
QuangHuy8299 <huypq234@gmail.com>
2022-04-15 15:21:30 +0700

    updateExpense

---
+++ components/UI/IconButton.js
@@ -0,0 +1,33 @@
+import { Ionicons } from "@expo/vector-icons";
+import { Pressable, StyleSheet, View } from "react-native";
+
+function IconButton({ icon, size, color, onPress }) {
+  return (
+    <Pressable
+      onPress={onPress}
+      style={(pressed) =>
+        pressed
+          ? [styles.buttonContainer, styles.pressed]
+          : styles.buttonContainer
+      }
+    >
+      <View>
+        <Ionicons name={icon} size={size} color={color} />
+      </View>
+    </Pressable>
+  );
+}
+
+export default IconButton;
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    borderRadius: 24,
+    padding: 16,
+    marginHorizontal: 8,
+    marginVertical: 2,
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
